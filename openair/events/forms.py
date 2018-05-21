37a24fbfec6141850186edc15af6cced24babc3f
Andreas Schmid <ikeark@gmail.com>
2018-05-21 09:20:57 +0200

    Add event judo training

--- openair/events/forms.py
+++ openair/events/forms.py
@@ -20,8 +20,8 @@ class ParticipantDeleteForm(FlaskForm):
         return True
 
 
-class ParticipantForm(FlaskForm):
-    """Participant form."""
+class ParticipantFormJudoTurnier(FlaskForm):
+    """Participant form for Judo Turnier."""
 
     firstname = StringField('Vorname',
                             validators=[
@@ -57,11 +57,55 @@ class ParticipantForm(FlaskForm):
 
     def __init__(self, *args, **kwargs):
         """Create instance."""
-        super(ParticipantForm, self).__init__(*args, **kwargs)
+        super(ParticipantFormJudoTurnier, self).__init__(*args, **kwargs)
 
     def validate(self):
         """Validate the form."""
-        initial_validation = super(ParticipantForm, self).validate()
+        initial_validation = super(ParticipantFormJudoTurnier, self).validate()
+        if not initial_validation:
+            return False
+        return True
+
+
+class ParticipantFormJudoTraining(FlaskForm):
+    """Participant form for Judo Training."""
+
+    firstname = StringField('Vorname',
+                            validators=[
+                                DataRequired(message='Btte angeben'),
+                                Length(min=3, max=25)])
+    lastname = StringField('Nachname',
+                           validators=[
+                               DataRequired(message='Bitte angeben'),
+                               Length(min=3, max=25)])
+    sex = RadioField('Geschlecht', choices=[('m', 'Männlich'), ('w', 'Weiblich')])
+    birthday = IntegerField('Jahrgang',
+                            validators=[NumberRange(min=1990, max=2015, message='Zwischen 1900 und 2015'),
+                                        DataRequired(message='Bitte angeben')])
+    level = SelectField('Kyu/Dan',
+                        choices=[('6. Kyu', '6. Kyu'),
+                                 ('5. Kyu', '5. Kyu'),
+                                 ('4. Kyu', '4. Kyu'),
+                                 ('3. Kyu', '3. Kyu'),
+                                 ('2. Kyu', '2. Kyu'),
+                                 ('1. Kyu', '1. Kyu'),
+                                 ('1. Dan', '1. Dan'),
+                                 ('2. Dan', '2. Dan'),
+                                 ('3. Dan', '3. Dan'),
+                                 ('4. Dan', '4. Dan'),
+                                 ('5. Dan', '5. Dan'),
+                                 ('6. Dan', '6. Dan')
+                                 ])
+
+    remark = TextAreaField('Bemerkung', [])
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(ParticipantFormJudoTraining, self).__init__(*args, **kwargs)
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(ParticipantFormJudoTraining, self).validate()
         if not initial_validation:
             return False
         return True
--- openair/events/views.py
+++ openair/events/views.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
-from openair.events.forms import ParticipantDeleteForm, ParticipantForm
+from openair.events.forms import ParticipantDeleteForm, ParticipantFormJudoTurnier, ParticipantFormJudoTraining
 from openair.events.models import Participant
 from openair.extensions import db
 from openair.user.models import User
@@ -27,8 +27,8 @@ def events_overview():
 @login_required
 def judo_turnier():
     """Show participants for judo turnier."""
-    participants = Participant.query.join(User).filter((User.id == current_user.id) and
-                                                       (Participant.event == EVENT_JUDO_TURNIER)).all()
+    participants = Participant.query.filter_by(user_id=current_user.id,
+                                               event=EVENT_JUDO_TURNIER).all()
 
     form = ParticipantDeleteForm(request.form)
     return render_template('events/judo-turnier.html', form=form, participants=participants)
@@ -50,7 +50,7 @@ def judo_turnier_remove(id):
 @login_required
 def judo_turnier_application():
     """Show application form to apply for event."""
-    form = ParticipantForm(request.form)
+    form = ParticipantFormJudoTurnier(request.form)
     if form.validate_on_submit():
         Participant.create(event=EVENT_JUDO_TURNIER, lastname=form.lastname.data,
                            firstname=form.firstname.data, birthday=form.birthday.data,
@@ -64,3 +64,45 @@ def judo_turnier_application():
         flash_errors(form)
 
     return render_template('events/judo-turnier-application.html', form=form)
+
+
+@blueprint.route('/judo-training', methods=['GET'])
+@login_required
+def judo_training():
+    """Show participants for judo training."""
+    participants = Participant.query.filter_by(user_id=current_user.id,
+                                               event=EVENT_JUDO_TRAINING).all()
+    form = ParticipantDeleteForm(request.form)
+    return render_template('events/judo-training.html', form=form, participants=participants)
+
+
+@blueprint.route('/judo-training/<int:id>', methods=['POST'])
+@login_required
+def judo_training_remove(id):
+    """Remove participant from event judo training."""
+    form = ParticipantDeleteForm(request.form)
+    if form.validate_on_submit():
+        ret = Participant.query.filter_by(id=id).delete()
+        db.session.commit()
+        flash('id is {} ret={}'.format(id, ret), 'success')
+    return redirect(url_for('events.judo_training'))
+
+
+@blueprint.route('/judo-training-application', methods=['GET', 'POST'])
+@login_required
+def judo_training_application():
+    """Show application form to apply for event."""
+    form = ParticipantFormJudoTraining(request.form)
+    if form.validate_on_submit():
+        Participant.create(event=EVENT_JUDO_TRAINING, lastname=form.lastname.data,
+                           firstname=form.firstname.data, birthday=form.birthday.data,
+                           level=form.level.data, sex=form.sex.data,
+                           weight=None, remark=form.remark.data,
+                           user=current_user)
+        flash('Neuer Teilnehmer "{} {}" wurde erfolgreich erfasst.'.format(form.firstname.data,
+              form.lastname.data), 'success')
+        return redirect(url_for('events.judo_training'))
+    else:
+        flash_errors(form)
+
+    return render_template('events/judo-training-application.html', form=form)
